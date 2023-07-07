e83ce5129eaa64618e12460d18ea70b9c1a41655
Felicia Kyalo <feliciakyalo@gmail.com>
2023-07-07 19:12:30 +0300

    seller module

--- resources/views/sellerHome.blade.php
+++ resources/views/sellerHome.blade.php
@@ -1,17 +1,50 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Seller Panel</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="{{asset('css/admin_style.css')}}">
    
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-   
-                <div class="card-body">
-                    You are a Seller User.
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+
+</head>
+
+@include('sellerHeader');
+<body>
+
+<!-- admin dashboard section starts  -->
+
+<section class="dashboard">
+
+   <h1 class="title">dashboard</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <p><a href="{{ route('create.Auction') }}">Add Auction</a></p>
+      </div>
+
+     <div class="box">
+        <p>Products List</p>
+     </div>
+
+     <div class="box">
+        <p>New Messages</p>
+     </div>
+
+   </div>
+
+</section>
+
+
+<!-- custom admin js file link  -->
+<script src="js/admin_script.js"></script>
+
+</body>
+</html>
